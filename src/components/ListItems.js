aa24a38df52cee651d9c00788ab38bc8a3a4d9c0
Amit Roy <amitroygcettb15@gmail.com>
2020-11-25 12:13:23 +0530

    adding files

---
+++ src/components/ListItems.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./ListItems.css";
+
+function ListItems(props) {
+  const items = props.items;
+  const listItems = items.map((item) => {
+    return (
+      <div className="list" key={item.key}>
+        <p>
+          {item.text}
+          <span>
+            <FontAwesomeIcon
+              className="faicons"
+              icon="trash"
+              onClick={() => props.deleteItem(item.key)}
+            />
+          </span>
+        </p>
+      </div>
+    );
+  });
+  return <div>{listItems}</div>;
+}
+
+export default ListItems;
